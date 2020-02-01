c9987121661828a0b2b7ddfa331b421ee476d37c
PrivateName <9034459505@mail.ru>
2020-02-02 02:45:25 +0300

    section carousel end

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        // adaptiveHeight: true,
+        speed: 800,
+        prevArrow: '<button type="button" class="slick-prev"><img src = "../img/sliders/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src = "../img/sliders/right.svg"></button>',
+    });
+});
